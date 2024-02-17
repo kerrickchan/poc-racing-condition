05ca5da11853f2e7123f6b96bf7f353558dc4f46
Kerrick Chan <kerrickchan@icloud.com>
2024-02-17 21:30:12 +0800

    remove code unique constraint

--- server/internal/model/ticket.go
+++ server/internal/model/ticket.go
@@ -1,7 +1,7 @@
 package model
 
 type Ticket struct {
-	ID   uint   `gorm:"primaryKey;default:auto_random()"`
-	Code string `gorm:"unique;not null"`
+	ID   uint `gorm:"primaryKey;default:auto_random()"`
+	Code string
 	User string
 }
